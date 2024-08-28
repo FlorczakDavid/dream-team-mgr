173fb8f0c011d2965968b79cc23f5218d83bdc1d
Rika Kishiro <rika.kishiro@gmail.com>
2024-08-28 18:02:08 +0200

    [#5] clean files and create Header and Project form project form is in progress

--- dt-presentation/src/App.vue
+++ dt-presentation/src/App.vue
@@ -1,48 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-
+  <header>
+    <Header />
+  </header>
   <main>
-    <TheWelcome />
+    <FormProject />
   </main>
 </template>
 
+<script>
+import FormProject from './components/FormProject.vue';
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    FormProject
+  }
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
