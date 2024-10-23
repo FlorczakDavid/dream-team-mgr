1bc4e1ce208b7759e24775c1ad6ced9770559506
David FLORCZAK <florczakdavid@gmail.com>
2024-10-23 17:33:23 +0200

    updated DML

--- dt-database/Scripts/data.dml.sql
+++ dt-database/Scripts/data.dml.sql
@@ -1,6 +1,14 @@
 DELETE FROM t_projects;
 DELETE FROM t_languages_technologies;
 
-INSERT INTO t_projects(name, unique_internal_identifier , start_date , description) VALUES ('Amazing project', 'PRO-123', '2024-8-27'::Date, 'Amazing project by dream team');
+INSERT INTO t_projects(name, unique_internal_identifier , start_date , description) 
+VALUES ('Amazing project', 'PRO-001', '2024-8-27'::Date, 'Amazing project by dream team'),
+VALUES ('Superb project 2', 'PRO-002', '2024-8-27'::Date, 'Superb project by dream team'),
+VALUES ('Magnificent project 3', 'PRO-003', '2024-8-27'::Date, 'Magnificent project by dream team'),
+VALUES ('素晴らしいプロジェクト 4', 'PRO-004', '2024-8-27'::Date, 'ドリームティームに造られた素晴らしいプロジェクト');
 
-INSERT INTO t_languages_technologies(name) VALUES ('JAVA');
+INSERT INTO t_languages_technologies(name) 
+VALUES ('JAVA'), ('JAVASCRIPT'), ('Kotlin'), ('Swift');
+
+INSERT INTO t_use(project_id, lang_tech_id) 
+VALUES (1, 1), (1, 2), (1, 3), (2, 1), (4, 3), (4, 4);
