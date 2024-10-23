345f83470af99f9db8eae6dcaf2a405272c76e7a
David FLORCZAK <florczakdavid@gmail.com>
2024-10-23 17:36:47 +0200

    updated DML-2

--- dt-database/Scripts/data.dml.sql
+++ dt-database/Scripts/data.dml.sql
@@ -3,9 +3,9 @@ DELETE FROM t_languages_technologies;
 
 INSERT INTO t_projects(name, unique_internal_identifier , start_date , description) 
 VALUES ('Amazing project', 'PRO-001', '2024-8-27'::Date, 'Amazing project by dream team'),
-VALUES ('Superb project 2', 'PRO-002', '2024-8-27'::Date, 'Superb project by dream team'),
-VALUES ('Magnificent project 3', 'PRO-003', '2024-8-27'::Date, 'Magnificent project by dream team'),
-VALUES ('素晴らしいプロジェクト 4', 'PRO-004', '2024-8-27'::Date, 'ドリームティームに造られた素晴らしいプロジェクト');
+('Superb project 2', 'PRO-002', '2024-8-27'::Date, 'Superb project by dream team'),
+('Magnificent project 3', 'PRO-003', '2024-8-27'::Date, 'Magnificent project by dream team'),
+('素晴らしいプロジェクト 4', 'PRO-004', '2024-8-27'::Date, 'ドリームティームに造られた素晴らしいプロジェクト');
 
 INSERT INTO t_languages_technologies(name) 
 VALUES ('JAVA'), ('JAVASCRIPT'), ('Kotlin'), ('Swift');
